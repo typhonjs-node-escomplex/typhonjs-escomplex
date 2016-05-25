fc6db89021e0e5f2e6413da1616f945d113e210f
typhonrt <support@typhonrt.org>
2016-05-25 04:57:07 -0700

    allow user supplied options to have sourceType set automatically.

--- src/parser.js
+++ src/parser.js
@@ -5,11 +5,11 @@ var espree = require('espree');
 var esmRegex = /(^\s*|[}\);\n]\s*)(import\s*(['"]|(\*\s+as\s+)?[^"'\(\)\n;]+\s*from\s*['"]|\{)|export\s+\*\s+from\s+["']|export\s* (\{|default|function|class|var|const|let|async\s+function))/;
 
 var espreeOptions = { loc: true, ecmaVersion: 6, ecmaFeatures: { jsx: true } };
-var espreeESMOptions = { loc: true, ecmaVersion: 6, sourceType: 'module', ecmaFeatures: { jsx: true } };
 
 exports.parse = parse;
 
 function parse (source, options) {
-    options = typeof options === 'object' ? options : esmRegex.test(source) ? espreeESMOptions : espreeOptions;
+    options = typeof options === 'object' ? options : espreeOptions;
+    options.sourceType = esmRegex.test(source) ? 'module' : 'script';
     return espree.parse(source, options);
 }
